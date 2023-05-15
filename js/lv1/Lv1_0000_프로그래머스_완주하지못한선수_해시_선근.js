6c70bf0e8b62e2be0e4219b8fd2a4a0907148b5e
이선근 <tjsrms1227@gmail.com>
2023-05-15 19:07:51 +0900

    Create Lv1_0000_프로그래머스_완주하지못한선수_해시_선근.js

---
+++ js/lv1/Lv1_0000_프로그래머스_완주하지못한선수_해시_선근.js
@@ -0,0 +1,7 @@
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) return participant[i];
+  }
+}
