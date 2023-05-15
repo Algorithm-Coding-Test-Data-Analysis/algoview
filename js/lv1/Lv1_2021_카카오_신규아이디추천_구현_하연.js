83d5f7df9d6c71ae9941f877b40bd31233242500
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-05-16 00:33:05 +0900

    Update Lv1_2021_카카오_신규아이디추천_구현_하연.js

--- js/lv1/Lv1_2021_카카오_신규아이디추천_구현_하연.js
+++ js/lv1/Lv1_2021_카카오_신규아이디추천_구현_하연.js
@@ -8,5 +8,3 @@ function solution(new_id) {
       .slice(0,15).replace(/\.$/, '');
   return new_id.length > 2 ? new_id : new_id + new_id[new_id.length - 1].repeat(3 - new_id.length);
 }
-
-// https://school.programmers.co.kr/learn/courses/30/lessons/72410
