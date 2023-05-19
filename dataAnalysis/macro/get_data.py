2330c1faddffe71f6abc65fdc054d65a6092350b
Jinchan Kim <97337454+multihalbun@users.noreply.github.com>
2023-05-19 18:25:12 +0900

    하위폴더까지 .js .py인 파일만 수집

--- dataAnalysis/macro/get_data.py
+++ dataAnalysis/macro/get_data.py
@@ -1,5 +1,45 @@
 import os
 
+def get_data_from_file(path, file_name) -> dict:
+    '''
+    data 폴더 내에 모든 파일의 데이터를 수집하여
+    dict형태로 만든 후 list로 묶어 반환합니다.
+
+    Args:
+        path (str): 파일의 경로
+        file_name: 파일의 이름
+
+    Returns:
+        file_data (dict): 파일의 데이터
+    '''
+    f = open(path+'/'+file_name, 'r', encoding='utf-8')
+    code_str = ''
+    line_ctd = 0
+    while True:
+        line = f.readline()
+        if line == '\n':
+            continue
+        if not line:
+            break
+        code_str += line
+        line_ctd += 1
+
+    name_split = file_name.split('_')
+    file_data = {
+        "file_name" : file_name,
+        "level":"level"+name_split[0][-1],
+        "year":name_split[1],
+        "company_name":name_split[2],
+        "problem_name" : name_split[3],
+        "problem_type" : name_split[4],
+        "countmethod": None,
+        "line_count":line_ctd,
+        "module": None,
+        "code": code_str,
+    }
+    f.close
+    return file_data
+
 def get_data() -> list[dict]:
     '''
     data 폴더 내에 모든 파일의 데이터를 수집하여
@@ -11,34 +51,11 @@ def get_data() -> list[dict]:
     Returns:
         data (list): 수집한 데이터 리스트
     '''
-    path = os.getcwd() + '/data/'
-    file_list = os.listdir(path)
+    dir_name = os.getcwd() + '/data/'
     data = []
-    for i in file_list:
-        f = open(path + i, 'r', encoding='utf-8')
-        code_str = ''
-        line_ctd = 0
-        while True:
-            line = f.readline()
-            if line == '\n':
-                continue
-            if not line:
-                break
-            code_str += line
-            line_ctd += 1
-
-        name_split = i.split('_')
-        data.append({
-            "file_name" : i,
-            "level":"level"+name_split[0][-1],
-            "year":name_split[1],
-            "company_name":name_split[2],
-            "problem_name" : name_split[3],
-            "problem_type" : name_split[4],
-            "countmethod": None,
-            "line_count":line_ctd,
-            "module": None,
-            "code": code_str,
-        })
-        f.close
+    for (path, dir, files) in os.walk(dir_name):
+        for file_name in files:
+            ext = os.path.splitext(file_name)[-1]
+            if ext == '.py' or ext == '.js':
+                data.append(get_data_from_file(path, file_name))
     return data
