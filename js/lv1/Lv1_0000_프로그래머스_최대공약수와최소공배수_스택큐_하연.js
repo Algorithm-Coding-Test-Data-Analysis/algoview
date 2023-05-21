c35ac929828ace60acfc7fe771f0fb1cead55f4d
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-05-21 17:19:49 +0900

    답안 수정
    다른 문제 답안으로 잘못 올려서, 해당 문제 답안으로 변경

--- js/lv1/Lv1_0000_프로그래머스_최대공약수와최소공배수_스택큐_하연.js
+++ js/lv1/Lv1_0000_프로그래머스_최대공약수와최소공배수_스택큐_하연.js
@@ -1,10 +1,8 @@
-function solution(cards1, cards2, goal) {
-    let a = 0,
-        b = 0;
-    for (const v of goal) {
-        if (cards1[a] === v) a++;
-        else if (cards2[b] === v) b++;
-        else return 'No';
-    }
-  return 'Yes';
+function solution(n, m) {
+  const GCD = findGCD(n, m);
+  const LCM = n * m / GCD;
+  return [GCD, LCM];
+}
+function findGCD(a, b) {
+  return a % b === 0 ? b : findGCD(b, a % b);
 }
