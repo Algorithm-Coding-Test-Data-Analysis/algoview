aa3afca0e1b25917efb9ddd16a73f73af3c74530
yeji_kim <116805856+yejify@users.noreply.github.com>
2023-05-22 21:53:06 +0900

    Create Lv0_0000_프로그래머스_문자열_출력하기_구현_예지.js

---
+++ data/js/lv0/Lv0_0000_프로그래머스_문자열_출력하기_구현_예지.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    console.log(str)
+});
