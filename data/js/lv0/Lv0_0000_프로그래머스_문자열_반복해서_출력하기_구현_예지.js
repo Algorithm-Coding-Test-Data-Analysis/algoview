84322e1753a31ed855e813194d8e1fe164b12408
yeji_kim <116805856+yejify@users.noreply.github.com>
2023-05-22 22:09:02 +0900

    Create Lv0_0000_프로그래머스_문자열_반복해서_출력하기_구현_예지.js

---
+++ data/js/lv0/Lv0_0000_프로그래머스_문자열_반복해서_출력하기_구현_예지.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str = input[0];
+    n = Number(input[1]);
+    console.log(str.repeat(n));
+});
