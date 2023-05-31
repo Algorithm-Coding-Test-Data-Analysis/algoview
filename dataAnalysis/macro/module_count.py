f89d293de5c83e7c853b7d165b97b9349c96c77d
nohheeyeon <scarletberries@naver.com>
2023-05-31 22:06:07 +0900

    module count 기능 추가

---
+++ dataAnalysis/macro/module_count.py
@@ -0,0 +1,21 @@
+from collections import Counter
+import re
+
+
+def module_count(data):
+    for file_data in data:
+        code = file_data["code"]
+
+        module_counts = Counter()
+
+        matches = re.findall(
+            r"(?:from|import)\s+([\w\s,]+?)(?:\s+as\s+\w+)?(?=\s*import|\n|$)", code
+        )
+
+        for match in matches:
+            modules = [m.strip() for m in match.split(",")]
+            module_counts.update(modules)
+
+        file_data["module"] = dict(module_counts)
+
+    return data
