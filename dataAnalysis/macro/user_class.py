982548803f4489e0b69884af27cc776947900e02
taencoding <xodms512@gmail.com>
2023-06-01 18:18:25 +0900

    user_class data 저장 추가

--- dataAnalysis/macro/user_class.py
+++ dataAnalysis/macro/user_class.py
@@ -4,5 +4,6 @@ import re
 def check_custom_functions(code_str) -> int:
     matches = re.findall(r"def\s+(\w+)", code_str)
     has_custom_functions = 1 if matches else 0
+    file_data['user_class'] = has_custom_functions
 
-    return has_custom_functions
+    return file_data
