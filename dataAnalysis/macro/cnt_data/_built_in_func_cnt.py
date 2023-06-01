3233d4ddaa716a3cf31be24f3d331b1caf71f44a
kwangho <pokljh0411@gmail.com>
2023-06-01 19:02:10 +0900

    사용자 정의 class count빼기

--- dataAnalysis/macro/cnt_data/_built_in_func_cnt.py
+++ dataAnalysis/macro/cnt_data/_built_in_func_cnt.py
@@ -16,9 +16,19 @@ def insert_countmethod_data(full_data: list[dict]) -> list[dict]:
 
     data = {}
     language = ''
+    custom_class = []
+    custom_values = []
+    code_data = ''
     for i in full_data:
         language = i.get('file_name').split('.')[1]
-        data = get_method_cnt(i.get('code'), language)
+        code_data = i.get('code')
+        custom_class = user_class_names(code_data)
+        # 사용자 정의 클래스가 있다면
+        if custom_class:
+            custom_values = class_value_names(code_data, custom_class)
+            code_data = remove_custom(code_data, custom_values)
+
+        data = get_method_cnt(code_data, language)
         i['countmethod'] = data if data else 0
     
     return full_data
@@ -58,3 +68,56 @@ def get_method_cnt(code_data: str, language: str = 'py') -> dict:
     pattern = f'({func_list_str}|[.][a-zA-Z0-9]+)\('
     p = re.compile(pattern)
     return dict(Counter(p.findall(code_data)))
+
+def user_class_names(code_data: str) -> list:
+    '''
+    file_data의 code 문자열 데이터를 받아
+    사용자가 정의한 class명을 리스트로 반환
+
+    Args:
+        code_data (str): 코드 문자열 데이터
+
+    Returns:
+        name_list (list): 사용자가 정의한 class명 리스트
+    '''
+
+    name_list = re.findall('class ([a-zA-Z0-9_]+)', code_data)
+    return name_list
+
+
+def class_value_names(code_data: str, class_names: list) -> list:
+    '''
+    file_data의 code 문자열 데이터와 class명 리스트를 받아
+    사용자가 정의한 class 변수명을 리스트로 반환
+
+    Args:
+        code_data (str): 코드 문자열 데이터
+        class_names (list): class명을 담은 리스트
+
+    Returns:
+        value_names (list): 사용자가 정의한 class변수명 리스트
+    '''
+
+    class_names_str = str.join('|', class_names)
+    pattern = f'([a-zA-Z0-9]+)\s?=\s?(?:new)? (?:{class_names_str})\('
+    value_names = re.findall(pattern, code_data)
+
+    return value_names
+
+def remove_custom(code_data: str, value_names: list) -> str:
+    '''
+    file_data의 code 문자열 데이터를 받아
+    사용자가 정의한 class 변수명들을 리스트로 반환
+
+    Args:
+        code_data (str): 코드 문자열 데이터
+        value_names (list): class 변수명을 담은 리스트
+
+    Returns:
+        remove_data (str): 사용자가 정의한 함수들 공백으로 제거한 코드 데이터
+    '''
+
+    value_names_str = str.join('|', value_names)
+    pattern = f'((?:{value_names_str})[.][a-zA-Z0-9]+)\('
+    remove_data = re.sub(pattern, ' ', code_data)
+    return remove_data
