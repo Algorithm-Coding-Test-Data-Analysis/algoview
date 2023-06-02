ed65b0c644fd60569316b820889a046f4af7d81a
yonainthefish <yonaisgood@gmail.com>
2023-06-03 01:48:18 +0900

    style:이미지경로수정

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,6 @@
 import { footerHTML } from '/src/js/footer.js';
-<<<<<<< HEAD
 import { headerHTML } from '/src/js/header.js';
 
 export { footerHTML };
 export { headerHTML };
 
-=======
-
-export { footerHTML };
->>>>>>> 06508c748809b566e0550dec0054c92828ea39af
--- src/js/header.js
+++ src/js/header.js
@@ -3,7 +3,7 @@ export function footerHTML() {
         <header>
             <h1>
                 <a href="#">
-                    <img class="top-logo" src="./images/algo-view-logo.svg" alt="알고뷰 로고">
+                    <img class="top-logo" src="/src/assets/images/ALGOVIEWlogo.png" alt="알고뷰 로고">
                 </a>
             </h1>
             <nav>
@@ -13,8 +13,8 @@ export function footerHTML() {
                 </ul>
                 <input type="checkbox" id="toggle" hidden>
                 <label for="toggle" class="dark-toggle">
-                    <img class="dark-toggle-img" src="./images/dark-mode.svg" alt="다크모드">
-                    <img class="light-toggle-img" src="./images/light-mode.svg" alt="밝은모드">
+                    <img class="dark-toggle-img" src="/src/assets/images/dark-mode.svg" alt="다크모드">
+                    <img class="light-toggle-img" src="/src/assets/images/light-mode.svg" alt="밝은모드">
                 </label>
             </nav>
         </header>
