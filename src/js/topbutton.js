1691618fd68565b1f4c7ea747923e8f10d22c16b
annasyun <yunjbly2470@naver.com>
2023-06-03 23:02:49 +0900

    fix: topbutton footer 위로 고정

--- src/js/topbutton.js
+++ src/js/topbutton.js
@@ -7,3 +7,20 @@ const scrollToTop = () => {
 
 const btnTop = document.querySelector('.btn-top');
 btnTop.addEventListener('click', scrollToTop);
+
+const footer = document.querySelector('.footer');
+const btnOffset = 80; // 버튼의 위치로부터의 거리 값
+
+window.addEventListener('scroll', function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var windowHeight = window.innerHeight;
+  var footerOffset = footer.offsetTop;
+
+  if (scrollTop + windowHeight >= footerOffset - btnOffset) {
+    btnTop.style.position = 'absolute';
+    btnTop.style.bottom = '80px';
+  } else {
+    btnTop.style.position = 'fixed';
+    btnTop.style.bottom = btnOffset + 'px';
+  }
+});
