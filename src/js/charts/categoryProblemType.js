dcb0b13ce8611e4944d41bc42a6cf8b282ce19dc
ebokyung <dlqhrud303@naver.com>
2023-06-04 16:04:40 +0900

    feat: problemTypeFunction 차트 구현

--- src/js/charts/categoryProblemType.js
+++ src/js/charts/categoryProblemType.js
@@ -1,9 +1,11 @@
 import getProblemTypeData from './problemTypeChart.js';
 import getProblemTypeMethod from './problemTypeMethodChart.js';
+import getProblemTypeFunctionData from './problemTypeFunctionChart.js';
 
 function getCategoryProblemType(props) {
   getProblemTypeData(...props);
   getProblemTypeMethod(...props);
+  getProblemTypeFunctionData(...props);
 }
 
 export default getCategoryProblemType;
