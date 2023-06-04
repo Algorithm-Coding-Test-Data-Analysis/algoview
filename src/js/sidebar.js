d3380d9e9026465797b0fb7f0e8fb8b5ded0a540
euiho.kim <kim-eh@naver.com>
2023-06-04 23:36:43 +0900

    feat: sidebar를 위한 코드 추가

---
+++ src/js/sidebar.js
@@ -0,0 +1,14 @@
+export function sideBarHTML() {
+  return `
+      <aside class="sidebar-menu-container">
+        <ul class="sidebar-menu-list">
+          <li><a href="#one" class="sidebar-menu-item">레벨</a></li>
+          <li><a href="#two" class="sidebar-menu-item">메서드</a></li>
+          <li><a href="#three" class="sidebar-menu-item">문제유형</a></li>
+          <li><a href="#four" class="sidebar-menu-item">출제 업체</a></li>
+          <li><a href="#five" class="sidebar-menu-item">라인 수</a></li>
+          <li><a href="#six" class="sidebar-menu-item">모듈</a></li>
+        </ul>
+      </aside>
+    `;
+}
