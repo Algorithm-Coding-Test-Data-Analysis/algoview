861fe182d7cbcc557a54e56e7f92f08ebc3de88c
baeggoose <bsj154@gmail.com>
2023-06-05 11:58:22 +0900

    common.js footer 함수실행설정, global.css와 footer.css 분리

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,7 @@
 import { footerHTML } from './footer.js';
 import { headerHTML, onClickHeader } from './header.js';
+
 headerHTML();
 onClickHeader();
+
+document.querySelector('.footer').innerHTML = footerHTML();
