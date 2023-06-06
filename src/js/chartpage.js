5cc12665cb8c9cfe97b183e69476d0a6590d934e
Kim Hayeon <k.hayeon00@gmail.com>
2023-06-06 15:20:47 +0900

    feat: 언어에 따라 캔버스 숨김 기능

--- src/js/chartpage.js
+++ src/js/chartpage.js
@@ -35,8 +35,46 @@ $pyBtn.addEventListener('click', () => {
   // lang = lang === 'py' ? 'js' : 'py';
   lang = 'py';
   updateCharts();
+  showOnlyPyCanvas();
 });
 $jsBtn.addEventListener('click', () => {
   lang = 'js';
   updateCharts();
+  showOnlyJsCanvas();
 });
+
+// py만 있는 차트
+const $pyOnlyChart1 = document.getElementById('problem-type-function-method-chart').parentNode;
+const $pyOnlyChart2 = document.getElementById('problem-type-function-chart').parentNode;
+// js만 있는 차트
+const $jsOnlyChart1 = document.getElementById('packJun-chart').parentNode;
+const $jsOnlyChart2 = document.getElementById('kakao-chart').parentNode;
+const $jsOnlyChart3 = document.getElementById('programmers-chart').parentNode;
+
+function showOnlyPyCanvas() {
+  $jsOnlyChart1.classList.add('hidden');
+  $jsOnlyChart2.classList.add('hidden');
+  $jsOnlyChart3.classList.add('hidden');
+
+  $pyOnlyChart1.classList.remove('hidden');
+  $pyOnlyChart2.classList.remove('hidden');
+}
+
+function showOnlyJsCanvas() {
+  $jsOnlyChart1.classList.remove('hidden');
+  $jsOnlyChart2.classList.remove('hidden');
+  $jsOnlyChart3.classList.remove('hidden');
+
+  $pyOnlyChart1.classList.add('hidden');
+  $pyOnlyChart2.classList.add('hidden');
+}
+
+// 언어 선택 토글로 변경 시, 이 함수로 변경
+function toggleCanvasHidden() {
+  $jsOnlyChart1.classList.toggle('hidden');
+  $jsOnlyChart2.classList.toggle('hidden');
+  $jsOnlyChart3.classList.toggle('hidden');
+
+  $pyOnlyChart1.classList.toggle('hidden');
+  $pyOnlyChart2.classList.toggle('hidden');
+}
