731a907607972c7cc438fffbb157cd1bad0f3efb
itso-wavy <you.meet.vvavy@gmail.com>
2023-06-08 06:54:58 +0900

    feat: chartPage btn-top darkmode 적용

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,4 +1,4 @@
-const COMPONENTS_CLASS = ['.header', '.sidebar', '.main', '.footer'];
+const COMPONENT_CLASSES = ['.header', '.sidebar', '.main', '.footer'];
 
 let isDarkMode = sessionStorage.getItem('algoview')
   ? JSON.parse(sessionStorage.getItem('algoview'))
@@ -7,19 +7,23 @@ let isDarkMode = sessionStorage.getItem('algoview')
 function renderDarkMode($darkmode) {
   const headerLogo = document.querySelector('.header-logo');
   const footerLogo = document.querySelector('.logo-img');
+  const topBtnLogo = document.querySelector('.btn-top');
 
+  console.dir(topBtnLogo.style);
   if (isDarkMode) {
     $darkmode.checked = true;
     headerLogo.src = '/src/assets/images/ALGOVIEW-headerlogo_dark.svg';
     footerLogo.src = '/src/assets/images/ALGOVIEWlogo_dark.png';
-    COMPONENTS_CLASS.forEach((component) =>
+    topBtnLogo.style.background = `url('/src/assets/images/btn-top_dark.svg')`;
+    COMPONENT_CLASSES.forEach((component) =>
       document.querySelector(component).classList.add('dark')
     );
   } else {
     $darkmode.checked = false;
     headerLogo.src = '/src/assets/images/ALGOVIEW-headerlogo.svg';
     footerLogo.src = '/src/assets/images/ALGOVIEWlogo.png';
-    COMPONENTS_CLASS.forEach((component) =>
+    topBtnLogo.style.background = `url('/src/assets/images/btn-top.svg')`;
+    COMPONENT_CLASSES.forEach((component) =>
       document.querySelector(component).classList.remove('dark')
     );
   }
