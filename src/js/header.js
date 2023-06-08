81e11a9e4cef4c77df36df2a30def88ee278d723
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-06-09 02:20:37 +0900

    feat: 햄버거 메뉴 구현

--- src/js/header.js
+++ src/js/header.js
@@ -20,13 +20,93 @@ export function headerHTML() {
                 'on-click'
               }"><a class="header-link" href="/pages/reference/youtube">Reference</a></li>
           </ul>
-          <input type="checkbox" id="toggle" hidden>
+          <input class="dark-toggle-input" type="checkbox" id="toggle" hidden>
           <label for="toggle" class="dark-toggle">
               <img class="dark-toggle-img" src="/src/assets/images/dark-mode.svg" alt="다크모드">
               <img class="light-toggle-img" src="/src/assets/images/light-mode.svg" alt="밝은모드">
           </label>
+          <button class="btn-hamburger" type="button">
+              <span></span>
+              <span></span>
+              <span></span>
+          </button>
+
+          <div class="overlay">
+            <div class="hidden-menu-bar">
+              <div class="hidden-menu-wrapper">
+                <img class="hidden-menu-logo" src="/src/assets/images/ALGOVIEWlogo.png"/>
+                <input class="hidden-dark-toggle-input" type="checkbox" id="hiddenToggle" hidden>
+                <label for="hiddenToggle" class="hidden-dark-toggle">
+                  <img class="hidden-dark-toggle-img" src="/src/assets/images/dark-mode.svg" alt="다크모드">
+                  <img class="hidden-light-toggle-img" src="/src/assets/images/light-mode.svg" alt="밝은모드">
+                </label>
+              </div>
+
+              <div class="hidden-menu">
+                <ul class="hidden-header-menu">
+                  <li class="hidden-header-menu-chart ${
+                    currentPath === '/' && 'on-click'
+                  }"><a class="hidden-header-link" href="/">Chart</a></li>
+                  <li class="hidden-header-menu-reference ${
+                    (currentPath === '/pages/reference/youtube/' ||
+                      currentPath === '/pages/reference/practice/') &&
+                    'on-click'
+                  }"><a class="hidden-header-link" href="/pages/reference/youtube">Reference</a></li>
+                  <li class="hidden-header-menu-github">
+                    <a class="hidden-header-link" href="https://github.com/Algorithm-Coding-Test-Data-Analysis/algoview">Github</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
       </nav>
     </div>
     `;
   return;
+}
+
+export function hamburger() {
+  const BtnHamburger = document.querySelector('.btn-hamburger');
+  const hiddenMenuBar = document.querySelector('.hidden-menu-bar');
+  const overlay = document.querySelector('.overlay');
+
+  // 햄버거 버튼 클릭 이벤트 리스너
+  BtnHamburger.addEventListener('click', () => {
+    toggleMenu();
+  });
+
+  // 문서 클릭 이벤트 리스너
+  document.addEventListener('click', (event) => {
+    const target = event.target;
+    const isMenuClicked = target.closest('.hidden-menu-bar') !== null;
+    const isHamburgerClicked = target.closest('.btn-hamburger') !== null;
+
+    if (!isMenuClicked && !isHamburgerClicked) {
+      hideMenu();
+    }
+  });
+
+  // 메뉴 토글 함수
+  function toggleMenu() {
+    if (
+      hiddenMenuBar.style.display === 'none' ||
+      hiddenMenuBar.style.display === ''
+    ) {
+      showMenu();
+    } else {
+      hideMenu();
+    }
+  }
+
+  // 메뉴 표시 함수
+  function showMenu() {
+    hiddenMenuBar.style.display = 'block';
+    overlay.style.display = 'block';
+  }
+
+  // 메뉴 숨기기 함수
+  function hideMenu() {
+    hiddenMenuBar.style.display = 'none';
+    overlay.style.display = 'none';
+  }
 }
