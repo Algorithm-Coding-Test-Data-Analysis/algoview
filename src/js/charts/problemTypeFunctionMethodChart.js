e3bde8549fa3ab088118ceb7eb3040a1db14090c
itso-wavy <you.meet.vvavy@gmail.com>
2023-06-11 17:36:16 +0900

    feat: json 키 변경에 따라 JS 차트 problem_type_mothod => problem_type_function_method 로 변경

--- src/js/charts/problemTypeFunctionMethodChart.js
+++ src/js/charts/problemTypeFunctionMethodChart.js
@@ -52,6 +52,54 @@ function getProblemTypeFunctionMethodData(data, lang, charts) {
     );
 
     charts.push(problemTypeFunctionMethodChart);
+  } else if (lang === 'js') {
+    const problemType = Object.keys(data['js']['problem_type_function_method']);
+
+    const problemTypeFunctionData = Object.values(
+      data['js']['problem_type_function_method']
+    );
+    const allFunctions = [];
+    problemTypeFunctionData.forEach((i) => {
+      allFunctions.push(...Object.keys(i));
+    });
+    const functionList = new Set(allFunctions);
+
+    const datasets = [];
+    functionList.forEach((item) => {
+      datasets.push({
+        label: item,
+        data: [],
+      });
+    });
+    problemTypeFunctionData.forEach((el) => {
+      [...functionList].forEach((funcName, idx) => {
+        datasets[idx].data.push(el[funcName] || 0);
+      });
+    });
+
+    const problemTypeFunctionMethodChart = new Chart(
+      document.getElementById('problem-type-function-method-chart'),
+      {
+        type: 'bar',
+        data: {
+          labels: problemType,
+          datasets: datasets,
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              stacked: true,
+            },
+            y: {
+              stacked: true,
+            },
+          },
+        },
+      }
+    );
+
+    charts.push(problemTypeFunctionMethodChart);
   }
 }
 
