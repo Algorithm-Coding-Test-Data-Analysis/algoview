5294638241f9b22671d89a43c3fd3ee883193aeb
AMinSC <cdtjdcjf@gmail.com>
2023-06-14 10:55:36 +0900

    #옹알이(1), Action 테스트

---
+++ data/py/lv0/Lv0_0000_프로그래머스_옹알이 (1)_구현_성철.py
@@ -0,0 +1,9 @@
+def solution(babbling):
+    c = 0
+    for b in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+            if w * 2 not in b:
+                b = b.replace(w, ' ')
+        if len(b.strip()) == 0:
+            c += 1
+    return c
